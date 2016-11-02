06cf9963d2545c00269b2ab49297948a8ab8ed0d
elamre <e.vanrijnswou@innoseis.com>
2016-11-02 14:32:16 +0100

    Initial project

---
+++ android/src/com/elmar/space/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.elmar.space;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.elmar.space.Space;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Space(), config);
+	}
+}
---
+++ desktop/src/com/elmar/space/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.elmar.space.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.elmar.space.Space;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Space(), config);
+	}
+}
